81612b26960c13e7371fec34425ec664177ae8be
subikstha <subsatch@gmail.com>
2026-07-06 17:35:57 +0545

    feat: vanilla react components

--- lessons/padre-ginos/src/App.js
+++ lessons/padre-ginos/src/App.js
@@ -1,9 +1,22 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
+  return React.createElement("div", {}, [
     React.createElement("h1", {}, "Padre Gino's"),
-  );
+    React.createElement(Pizza, {
+      name: "The pepperoni Pizza",
+      description: "Some dope pizza yo",
+    }),
+    React.createElement(Pizza, {
+      name: "Americano Pizza",
+      description: "French fries and hot dogs",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
