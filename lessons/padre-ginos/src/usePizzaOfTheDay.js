d1aebbcddd022888d77b6272e370f17cb63465ba
subikstha <subsatch@gmail.com>
2026-07-07 17:01:51 +0545

    feat: added useDebugValue hook

--- lessons/padre-ginos/src/usePizzaOfTheDay.js
+++ lessons/padre-ginos/src/usePizzaOfTheDay.js
@@ -1,7 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useDebugValue } from "react";
 
 const usePizzaOfTheDay = () => {
   const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+  useDebugValue(
+    pizzaOfTheDay ? `${pizzaOfTheDay.id}: ${pizzaOfTheDay.name}` : "loading...",
+  );
   const fetchPizzaOfTheDay = async () => {
     const pizzaResponse = await fetch("/api/pizza-of-the-day");
     const pizzaData = await pizzaResponse.json();
