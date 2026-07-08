6389bf2c33dbb438e99b3a98aa2e7c61a536e1da
subikstha <subsatch@gmail.com>
2026-07-08 17:38:38 +0545

    feat: lazy loaded routes

--- lessons/padre-ginos/src/routes/order.lazy.jsx
+++ lessons/padre-ginos/src/routes/order.lazy.jsx
@@ -1,14 +1,19 @@
 import { useState, useEffect, useContext } from "react";
+import { createLazyFileRoute } from "@tanstack/react-router";
 import Pizza from "../Pizza";
 import Cart from "../Cart";
 import { CartContext } from "../contexts";
+
+export const Route = createLazyFileRoute("/order")({
+  component: Order,
+});
 
 const intl = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
 });
 
-export default function Order() {
+function Order() {
   const [pizzaTypes, setPizzaTypes] = useState([]);
   const [pizzaType, setPizzaType] = useState("pepperoni");
   const [pizzaSize, setPizzaSize] = useState("M");
