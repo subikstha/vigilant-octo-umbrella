cf4723ce87bebf238db6d66ceb84512717b6b7da
subikstha <subsatch@gmail.com>
2026-07-08 18:21:45 +0545

    feat: added tanstack query

--- lessons/padre-ginos/src/App.jsx
+++ lessons/padre-ginos/src/App.jsx
@@ -1,12 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 };
 
 const container = document.getElementById("root");
