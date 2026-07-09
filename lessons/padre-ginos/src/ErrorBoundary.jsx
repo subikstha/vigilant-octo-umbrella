3f124704fc0be5cd327d6c375b581b01c8a04bf2
subikstha <subsatch@gmail.com>
2026-07-09 18:10:07 +0545

    feat: ErrorBoundary component

---
+++ lessons/padre-ginos/src/ErrorBoundary.jsx
@@ -0,0 +1,34 @@
+import { Link } from "@tanstack/react-router";
+import { Component } from "react";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {
+    console.error("Errorbounday caught some stupid error", error, info);
+  }
+  componentDidMount() {
+    // like useEffect with empty dependency array
+  }
+  componentWillUnmount() {
+    // like useEffect clean up function
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="error-boundary">
+          <h2>Uh oh!</h2>
+          <p>
+            There was an error with this page. <Link to="/">Click here</Link> to
+            go back to the home page.
+          </p>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
