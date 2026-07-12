ed064260767c681985817763d22dedcf911c1867
subikstha <subsatch@gmail.com>
2026-07-12 20:05:22 +0545

    feat: added a snapshot test

--- lessons/padre-ginos/src/__tests__/usePizzaOfTheDay.test.jsx
+++ lessons/padre-ginos/src/__tests__/usePizzaOfTheDay.test.jsx
@@ -1,5 +1,5 @@
 import { expect, test, vi } from "vitest";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import createFetchMock from "vitest-fetch-mock";
 import usePizzaOfTheDay from "../usePizzaOfTheDay";
 
@@ -19,4 +19,14 @@ test("gives null when first called", async () => {
   fetch.mockResponse(JSON.stringify(testPizza));
   const { result } = renderHook(() => usePizzaOfTheDay());
   expect(result.current).toBeNull();
+});
+
+test("to call the API and give back the pizza of the day", async () => {
+  fetch.mockResponse(JSON.stringify(testPizza));
+  const { result } = renderHook(() => usePizzaOfTheDay());
+  await waitFor(() => {
+    expect(result.current).toEqual(testPizza);
+  });
+
+  expect(fetchMocker).toBeCalledWith("/api/pizza-of-the-day");
 });
