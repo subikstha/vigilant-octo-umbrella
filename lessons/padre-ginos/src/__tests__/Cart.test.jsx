cfcea9a59c286db0a28a996296654341332d7908
subikstha <subsatch@gmail.com>
2026-07-12 20:13:14 +0545

    feat: added a snapshot test

--- lessons/padre-ginos/src/__tests__/Cart.test.jsx
+++ lessons/padre-ginos/src/__tests__/Cart.test.jsx
@@ -5,4 +5,16 @@ import Cart from "../Cart";
 test("snapshot with nothing in cart", () => {
   const { asFragment } = render(<Cart cart={[]} />);
   expect(asFragment()).toMatchSnapshot();
+  //   expect(asFragment()).toMatchInlineSnapshot()
+});
+
+test("lol", () => {
+  expect({
+    id: "calabrese",
+    name: "The Calabrese Pizza",
+    category: "Supreme",
+    description: "lol pizza from Calabria",
+    image: "/public/pizzas/calabrese.webp",
+    size: { S: 12.25, M: 16.25, L: 20.25 },
+  }).toMatchSnapshot();
 });
